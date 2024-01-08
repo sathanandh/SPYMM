3392af42cffce063851d1beb9f7ddb76c3e3fed5
sathanandh <sathanandh.c@gmail.com>
2024-01-08 07:42:58 +0000

    Code Synced by DhiWise

---
+++ app/src/test/java/com/sathanandhsapplication/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.sathanandhsapplication.app
+
+import com.sathanandhsapplication.app.appcomponents.base.BaseActivity
+import com.sathanandhsapplication.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
